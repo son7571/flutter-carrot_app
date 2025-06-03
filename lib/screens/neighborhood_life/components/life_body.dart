8a8b233874cccb38a59133167fb0166904fe2e53
son <sssk1755@gmail.com>
2025-06-03 16:56:14 +0900

    동네생활(사진제외)완)

--- lib/screens/neighborhood_life/components/life_body.dart
+++ lib/screens/neighborhood_life/components/life_body.dart
@@ -1,11 +1,159 @@
+import 'package:carrot_app/models/neighborhood_life.dart';
+import 'package:carrot_app/screens/chatting/components/image_container.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LifeBody extends StatelessWidget {
-  final NeighborhoodLife neighborhoodLife
-  const LifeBody({Key? key, required this.neighborhoodLife}) ;super
+  final NeighborhoodLife neighborhoodLife;
+
+  const LifeBody({Key? key, required this.neighborhoodLife}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card();
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 0.5, color: Color(0xFFD4D5DD)),
+        ),
+      ),
+      child: Column(
+        children: [
+          _buildTop(),
+          _buildWriter(),
+          _buildWriting(),
+          _buildImage(),
+          Divider(
+            height: 1,
+            thickness: 1,
+            color: Colors.grey[300],
+          ),
+          _buildTail(neighborhoodLife.commentCount),
+        ],
+      ),
+    );
+  }
+
+  Padding _buildTop() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 16,
+      ),
+      child: Container(
+        padding: EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+          color: Color.fromRGBO(247, 247, 247, 1),
+        ),
+        child: Row(
+          children: [
+            Text(neighborhoodLife.category, style: TextTheme().bodyMedium),
+            Spacer(),
+            Text(
+              neighborhoodLife.date,
+              style: TextTheme().bodyMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding _buildWriter() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      child: Row(
+        children: [
+          ImageContainer(
+            borderRadius: 15,
+            imageUrl: neighborhoodLife.contentImgUri,
+            // profileImagUri는 이미지에러라 다른주소
+            width: 30,
+            height: 30,
+          ),
+          Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: "${neighborhoodLife.userName}",
+                  style: TextTheme().bodyMedium,
+                ),
+                TextSpan(text: "${neighborhoodLife.location}"),
+                TextSpan(text: "인증 ${neighborhoodLife.authCount}"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding _buildWriting() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 16,
+      ),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          neighborhoodLife.content,
+          style: TextTheme().bodyLarge,
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.start,
+        ),
+      ),
+    );
+  }
+
+  Visibility _buildImage() {
+    return Visibility(
+      visible: neighborhoodLife.contentImgUri != "",
+      child: Padding(
+        padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+        child: Image.network(
+          neighborhoodLife.contentImgUri,
+          height: 200,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Padding _buildTail(int commentCount) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: [
+          Icon(
+            FontAwesomeIcons.smile,
+            color: Colors.grey,
+            size: 22,
+          ),
+          SizedBox(width: 8),
+          Text(
+            "공감하기",
+            style: TextStyle(fontSize: 16, color: Colors.black),
+          ),
+          SizedBox(width: 22),
+          Icon(
+            FontAwesomeIcons.commentAlt,
+            color: Colors.grey,
+            size: 22,
+          ),
+          SizedBox(width: 8),
+          Text(
+            "${"댓글쓰기"}$commentCount",
+            style: TextStyle(fontSize: 16, color: Colors.black),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/neighborhood_life/neighborhood_life_screen.dart
+++ lib/screens/neighborhood_life/neighborhood_life_screen.dart
@@ -1,5 +1,8 @@
+import 'package:carrot_app/models/neighborhood_life.dart';
 import 'package:carrot_app/screens/chatting/components/appbar_bottom_line.dart';
+import 'package:carrot_app/screens/neighborhood_life/components/life_body.dart';
 import 'package:carrot_app/screens/neighborhood_life/components/life_header.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NeighborhoodLifeScreen extends StatelessWidget {
@@ -11,15 +14,27 @@ class NeighborhoodLifeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("동네생활"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.search)),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.plus_rectangle_on_rectangle),
+          ),
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bell)),
+        ],
         bottom: appbarBottomLine(),
       ),
       body: ListView(
         children: [
           LifeHeader(),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 12),
-            // child: LifeBody(
-            //   neighborhoodLife: neighborhoodLifeList[0],)
+          ...List.generate(
+            neighborhoodLifeList.length,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: LifeBody(
+                neighborhoodLife: neighborhoodLifeList[index],
+              ),
+            ),
           ),
         ],
       ),
